51a001bdc69e20f07d9c397800a7d7110fa3ae7a
wali55 <walisantunu@gmail.com>
2023-07-18 20:29:18 +0600

    adding productModel

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import products from "./data/products.js";
+import connectDB from "./config/db.js";
 dotenv.config();
+
+connectDB();
 
 const port = process.env.PORT || 5000;
 
